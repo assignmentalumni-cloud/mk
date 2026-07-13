3f35b939631e8a8630fdcf31a950cc7b3a5d44d9
assignmentalumni-cloud <assignmentalumni@gmail.com>
2026-07-12 17:55:53 -0700

    Create announcement.tsx

---
+++ src/pages/announcement.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Announcement() {
+  return (
+    
+      Latest Announcements
+
+      
+        
+          🔥 Special Bonus Campaign
+        
+
+        
+          Refer 1 User = Get $5 Reward
+        
+        
+        
+          Invite your friends to join **Assignment Alumni**. The moment they register and activate their account, an instant $5 bonus will be credited straight to your wallet dashboard!
+        
+
+        
+          No limits on invitations. More referrals = More earnings! 🚀
+        
+      
+    
+  );
+}
